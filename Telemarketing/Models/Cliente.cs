12af3c79858ae185ca65c15727d5064d2df3ef7a
Alex Alves Pantolfo <alexpantolfo619@gmail.com>
2023-05-29 19:10:36 -0300

    Add project files.

---
+++ Telemarketing/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Telemarketing.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Column("Id")]
+        [Display(Name = "ID")]
+        public long id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string nome { get; set; }
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        [Column("Numero")]
+        [Display(Name = "Numero")]
+        public int numero { get; set; }
+    }
+}
